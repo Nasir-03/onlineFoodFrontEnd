20d59d589df7d11706ac5da560d85bcec564faa5
= <nasirequbal620@gmail.com>
2025-10-08 10:00:51 +0530

    fixed store.js problem

--- src/component/state/Store.js
+++ src/component/state/Store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import jwtReducer from "./jwtSlice";
-import userReducer from "./userSlice";
+import jwtReducer from "./JwtSlice";
+import userReducer from "./UserSlice";
 import favouriteReducer from "./FavouriteSlice"
 import cartReducer from "./CartSlice"
 
